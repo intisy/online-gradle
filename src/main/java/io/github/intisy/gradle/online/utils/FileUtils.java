f509f1e141a27e6319410bc81e35b335c5122d8f
Pizza <wilderpizza99@gmail.com>
2024-08-24 12:37:44 +0200

    fix: file could not be saved

--- src/main/java/io/github/intisy/gradle/online/utils/FileUtils.java
+++ src/main/java/io/github/intisy/gradle/online/utils/FileUtils.java
@@ -2,13 +2,26 @@ package io.github.intisy.gradle.online.utils;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class FileUtils {
     /**
      * download file from url {@param fileURL} and return the file {@return file}
      */
+    public static String generateUniqueString(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
+        } catch (NoSuchAlgorithmException var3) {
+            throw new RuntimeException("SHA-256 algorithm not found", var3);
+        }
+    }
     public static File downloadFile(String fileURL) {
-        File jar = new File(GradleUtils.getGradleHome().toFile(), fileURL);
+        File jar = new File(GradleUtils.getGradleHome().toFile(), generateUniqueString(fileURL));
         if (jar.exists())
             jar.delete();
         System.out.println("Downloading " + fileURL + " to " + jar.getAbsolutePath());
